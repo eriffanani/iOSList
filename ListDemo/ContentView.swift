b529d6c0701c0b6fa8e7f2a98b2ae42e1d18c714
Mukhammad Erif Fanani <erifmoker@gmail.com>
2021-08-13 07:56:45 +0700

    Initial Commit

---
+++ ListDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListDemo
+//
+//  Created by ok fits on 13/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListDemo/ListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListDemoApp.swift
+//  ListDemo
+//
+//  Created by ok fits on 13/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
